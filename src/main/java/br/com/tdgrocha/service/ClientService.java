946026a200c41f1eab3ce2ad4c09c78996506717
Thiago Guimarães <thiago.grocha@saganet.com.br>
2020-10-17 23:21:14 -0300

    Cria e estrutura projeto api

--- src/main/java/br/com/tdgrocha/service/ClientService.java
+++ src/main/java/br/com/tdgrocha/service/ClientService.java
@@ -3,18 +3,21 @@ package br.com.tdgrocha.service;
 import br.com.tdgrocha.pojos.ClientIn;
 import br.com.tdgrocha.pojos.ClientOut;
 import br.com.tdgrocha.entities.Client;
-import br.com.tdgrocha.repositories.ClientRepository;
 
 import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @ApplicationScoped
 public class ClientService {
 
+    @Inject
+    EntityManager em;
+
     public void save(ClientIn in) {
         try {
-            ClientRepository.getInstance().persist(new Client(null, in.getNome()));
+            em.persist(new Client(null, in.getNome()));
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -22,7 +25,7 @@ public class ClientService {
 
     public List<ClientOut> listAll() {
         try {
-            return ClientRepository.getInstance().findAll().parallelStream().map(entity -> new ClientOut(entity.getId(), entity.getNome())).collect(Collectors.toList());
+            return em.createQuery("FROM " + Client.class.getName(), ClientOut.class).getResultList();
         } catch (Exception e) {
             e.printStackTrace();
             return null;
